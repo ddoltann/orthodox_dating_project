4410fd04f290040f3a59b454277a75655f618ca0
ddoltann <130298406+ddoltann@users.noreply.github.com>
2025-09-05 06:25:17 +0300

    Замена PosgreSQL на SQLite

--- orthodox_dating/settings.py
+++ orthodox_dating/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'orthodox_dating.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'orthodox_dating',     # Имя вашей базы данных
-        'USER': 'orthodox_dating',         # Пользователь базы данных
-        'PASSWORD': 'orthodox_dating', # Пароль пользователя
-        'HOST': 'localhost',    # Хост (обычно 'localhost' или IP)
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
