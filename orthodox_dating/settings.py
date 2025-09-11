bd9c1d47048f1bc0086029abe3b02f12337a49ba
Alex <130298406+ddoltann@users.noreply.github.com>
2025-09-11 16:39:58 +0300

    Update settings.py

--- orthodox_dating/settings.py
+++ orthodox_dating/settings.py
@@ -84,7 +84,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -103,6 +102,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
+
 
 
 
