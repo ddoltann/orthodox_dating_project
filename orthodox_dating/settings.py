4c1d44781d98040a608080b69a1c87dcb182d5d0
Alex <130298406+ddoltann@users.noreply.github.com>
2025-09-11 17:06:54 +0300

    Update settings.py

--- orthodox_dating/settings.py
+++ orthodox_dating/settings.py
@@ -61,11 +61,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'orthodox_dating.wsgi.application'
 
 # Database
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 # Password validation
@@ -102,6 +104,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
+
 
 
 
