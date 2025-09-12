c77eb9e0e689ef2e257354c88721b4c85f00bfd3
Alex <130298406+ddoltann@users.noreply.github.com>
2025-09-12 07:44:59 +0300

    Update urls.py

--- orthodox_dating/urls.py
+++ orthodox_dating/urls.py
@@ -6,18 +6,16 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # Подключаем все URL-адреса из нашего приложения profiles
     path('', include('profiles.urls', namespace='profiles')),
-    
-    # URL для входа и выхода.
     path('login/', auth_views.LoginView.as_view(template_name='profiles/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='profiles/logged_out.html'), name='logout'),
 ]
 
-# Добавляем маршрут для раздачи медиа-файлов в режиме разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
 
 
 
