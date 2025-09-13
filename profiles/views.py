ffe1a1141e820b822555e8e0dbd4698a5b736e09
Alex <130298406+ddoltann@users.noreply.github.com>
2025-09-13 05:41:12 +0300

    Update views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -16,15 +17,20 @@ def home_page(request):
 
 def register(request):
     if request.method == 'POST':
-        user_form, profile_form = UserRegistrationForm(request.POST), UserProfileForm(request.POST, request.FILES)
+        user_form = UserRegistrationForm(request.POST)
+        profile_form = UserProfileForm(request.POST, request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password']); new_user.save()
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
             profile = profile_form.save(commit=False)
-            profile.user = new_user; profile.save()
+            profile.user = new_user
+            profile.save()
             messages.success(request, 'Регистрация прошла успешно! Теперь вы можете войти.')
             return redirect('login')
-    else: user_form, profile_form = UserRegistrationForm(), UserProfileForm()
+    else:
+        user_form = UserRegistrationForm()
+        profile_form = UserProfileForm()
     return render(request, 'profiles/register.html', {'user_form': user_form, 'profile_form': profile_form})
 
 @login_required
@@ -33,44 +39,57 @@ def profile_list(request):
     form = ProfileFilterForm(request.GET)
     if form.is_valid():
         cd = form.cleaned_data
-        if cd.get('gender'): profiles = profiles.filter(gender=cd['gender'])
-        if cd.get('city'): profiles = profiles.filter(city__icontains=cd['city'])
-        if cd.get('churching_level'): profiles = profiles.filter(churching_level=cd['churching_level'])
-        if cd.get('min_age'): profiles = profiles.filter(date_of_birth__year__lte=timezone.now().year - cd['min_age'])
-        if cd.get('max_age'): profiles = profiles.filter(date_of_birth__year__gte=timezone.now().year - cd['max_age'])
+        if cd.get('gender'):
+            profiles = profiles.filter(gender=cd['gender'])
+        if cd.get('city'):
+            profiles = profiles.filter(city__icontains=cd['city'])
+        if cd.get('churching_level'):
+            profiles = profiles.filter(churching_level=cd['churching_level'])
+        if cd.get('min_age'):
+            profiles = profiles.filter(date_of_birth__year__lte=timezone.now().year - cd['min_age'])
+        if cd.get('max_age'):
+            profiles = profiles.filter(date_of_birth__year__gte=timezone.now().year - cd['max_age'])
     return render(request, 'profiles/profile_list.html', {'profiles': profiles, 'form': form})
 
 @login_required
 def profile_detail(request, pk):
     profile_user = get_object_or_404(User, pk=pk)
-    mutual_like = Like.objects.filter(user_from=request.user, user_to=profile_user).exists() and \
-                  Like.objects.filter(user_from=profile_user, user_to=request.user).exists()
+    has_sent_like = Like.objects.filter(user_from=request.user, user_to=profile_user).exists()
+    has_received_like = Like.objects.filter(user_from=profile_user, user_to=request.user).exists()
+    mutual_like = has_sent_like and has_received_like
     return render(request, 'profiles/profile_detail.html', {'profile': profile_user.userprofile, 'mutual_like': mutual_like})
 
 @login_required
 def edit_profile(request):
     if request.method == 'POST':
-        if 'update_profile' in request.POST:
-            user_form = UserUpdateForm(request.POST, instance=request.user)
-            profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofile)
-            if user_form.is_valid() and profile_form.is_valid():
-                user_form.save(); profile_form.save()
-                messages.success(request, 'Ваш профиль был успешно обновлен!')
-                return redirect('profiles:edit_profile')
-        elif 'upload_photo' in request.POST:
-            photo_form = PhotoForm(request.POST, request.FILES)
-            if photo_form.is_valid():
-                photo = photo_form.save(commit=False); photo.user_profile = request.user.userprofile; photo.save()
-                messages.success(request, 'Фотография успешно добавлена!')
-                return redirect('profiles:edit_profile')
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Ваш профиль был успешно обновлен!')
+            return redirect('profiles:edit_profile')
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.userprofile)
     context = {
-        'user_form': user_form, 'profile_form': profile_form,
-        'photo_form': PhotoForm(), 'user_photos': request.user.userprofile.photos.all(),
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'photo_form': PhotoForm(),
+        'user_photos': request.user.userprofile.photos.all(),
     }
     return render(request, 'profiles/edit_profile.html', context)
+
+@login_required
+def upload_photo(request):
+    if request.method == 'POST':
+        photo_form = PhotoForm(request.POST, request.FILES)
+        if photo_form.is_valid():
+            photo = photo_form.save(commit=False)
+            photo.user_profile = request.user.userprofile
+            photo.save()
+            messages.success(request, 'Фотография успешно добавлена!')
+    return redirect('profiles:edit_profile')
 
 @login_required
 def delete_photo(request, photo_id):
@@ -87,30 +106,40 @@ def add_like(request, pk):
     if created:
         messages.success(request, f'Вы выразили симпатию {user_to.first_name}.')
         Notification.objects.create(recipient=user_to, sender=request.user, message=f"{request.user.first_name} выразил(а) вам симпатию.", notification_type='LIKE')
-    else: messages.info(request, f'Вы уже выражали симпатию этому пользователю.')
+    else:
+        messages.info(request, f'Вы уже выражали симпатию этому пользователю.')
     return redirect('profiles:profile_detail', pk=pk)
 
 @login_required
 def inbox(request):
     sent_to = Message.objects.filter(sender=request.user).values_list('receiver', flat=True)
     received_from = Message.objects.filter(receiver=request.user).values_list('sender', flat=True)
-    interlocutors = User.objects.filter(id__in=set(list(sent_to) + list(received_from)))
+    interlocutors_ids = set(list(sent_to) + list(received_from))
+    interlocutors = User.objects.filter(id__in=interlocutors_ids).distinct()
     return render(request, 'profiles/inbox.html', {'interlocutors': interlocutors})
 
 @login_required
 def conversation_detail(request, pk):
     interlocutor = get_object_or_404(User, pk=pk)
-    if not (Like.objects.filter(user_from=request.user, user_to=interlocutor).exists() and Like.objects.filter(user_from=interlocutor, user_to=request.user).exists()):
+    has_mutual_like = Like.objects.filter(user_from=request.user, user_to=interlocutor).exists() and Like.objects.filter(user_from=interlocutor, user_to=request.user).exists()
+    if not has_mutual_like:
         messages.error(request, 'Вы можете писать сообщения только пользователям с взаимной симпатией.')
         return redirect('profiles:profile_detail', pk=pk)
-    messages_list = Message.objects.filter( (Q(sender=request.user, receiver=interlocutor) | Q(sender=interlocutor, receiver=request.user)) ).order_by('timestamp')
+    
+    messages_list = Message.objects.filter(Q(sender=request.user, receiver=interlocutor) | Q(sender=interlocutor, receiver=request.user)).order_by('timestamp')
+    
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
-            message = form.save(commit=False); message.sender = request.user; message.receiver = interlocutor; message.save()
+            message = form.save(commit=False)
+            message.sender = request.user
+            message.receiver = interlocutor
+            message.save()
             Notification.objects.create(recipient=interlocutor, sender=request.user, message=f"Новое сообщение от {request.user.first_name}.", notification_type='MESSAGE')
             return redirect('profiles:conversation_detail', pk=pk)
-    else: form = MessageForm()
+    else:
+        form = MessageForm()
+    
     return render(request, 'profiles/conversation_detail.html', {'interlocutor': interlocutor, 'messages_list': messages_list, 'form': form})
 
 @login_required
